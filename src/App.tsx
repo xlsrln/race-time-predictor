38ece3e0ae6ffe271333a174b2953c8053249774
xlsrln <31051075+xlsrln@users.noreply.github.com>
2025-05-24 20:37:07 +0200

    try fixing

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={VITE_BASE_PATH}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
