9caa5442169c7662c4ca3a4f47790e0ba3a48412
xlsrln <31051075+xlsrln@users.noreply.github.com>
2025-05-24 20:37:58 +0200

    undo

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={VITE_BASE_PATH}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
